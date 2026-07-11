5e75121b2cadf0d0007a31bef58a180dc0c681c5
Pranay Dutta <rick15176@gmail.com>
2026-07-11 19:03:19 +0000

    create: impletement copy_files_recursive function to copy files and folder from source to dest folder

--- src/main.py
+++ src/main.py
@@ -1,13 +1,18 @@
 from textnode import TextNode, TextType
+from copy_static import copy_files_recursive
+import os
+import shutil
+
+static_path = "./static"
+public_path = "./public"
 
 
 def main():
-    text = "This is some anchor text"
-    text_type = TextType.LINK
-    url = "https://www.boot.dev"
-
-    textnode = TextNode(text=text, text_type=text_type, url=url)
-    print(textnode)
+    print("Deleting public directory...")
+    if os.path.exists(public_path):
+        shutil.rmtree(public_path)
+    print("Copying static files to public directory.")
+    copy_files_recursive(static_path, public_path)
 
 
 if __name__ == "__main__":
